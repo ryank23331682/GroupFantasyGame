1ea158774f3c3855530127ddc2057d54ca7d4a6f
Varad Lele <vrdcr7@gmail.com>
2023-11-28 15:22:07 +0530

    test 3

--- GroupFantasyGame/Inventory.cpp
+++ GroupFantasyGame/Inventory.cpp
@@ -1,4 +1,21 @@
-class Inventory
-{
-	Inventory();
+#include<vector>
+#include "Item.h"
+#include<iostream>
+class Inventory {
+private:
+	std::vector<Item> items;
+
+public:
+	void addItem(Item item) {
+		items.push_back(item);
+	}
+
+	void displayInventory() {
+		std::cout << "Inventory:\n";
+		for (const auto& item : items) {
+			std::cout << "Name: " << item.name << ", Category: " << item.category << ", Weight: " << item.weight << "\n";
+		}
+	}
+
+	// Additional methods for handling inventory and character interactions could be added here
 };
--- GroupFantasyGame/Item.h
+++ GroupFantasyGame/Item.h
@@ -1,5 +1,10 @@
-#pragma once
-class Item
-{
-};
+#include<string>
+using namespace std;
+class Item {
+public:
+	std::string name;
+	std::string category;
+	int weight;
 
+	Item(std::string n, std::string cat, int w) : name(n), category(cat), weight(w) {}
+};
