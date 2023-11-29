7d11e242366eb2548161aba071b0d60ecc38c612
Varad Lele <vrdcr7@gmail.com>
2023-11-29 17:11:10 +0530

    z

--- GroupFantasyGame/Player.cpp
+++ GroupFantasyGame/Player.cpp
@@ -6,7 +6,5 @@ public:
 	Player(std::string n, int hp, int att, int def)
 		: Character(n, hp, att, def), gold(0) {}
 
-	int calculateDamage() const override {
-		return 5; // Player's special ability: always deals 5 extra damage
-	}
+
 };
