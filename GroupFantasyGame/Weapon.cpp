8e1f1dcde6f970b5ad6a9c3e8c70e38a3a3b7e4e
Kevin Chesser <11137487@studentmail.ul.ie>
2023-12-04 14:48:57 +0000

    Y

--- GroupFantasyGame/Weapon.cpp
+++ GroupFantasyGame/Weapon.cpp
@@ -2,6 +2,14 @@
 #include <string>
 #include <iostream>
 
+/*
+* Weapon(string name, int weight, int aB)
+* Computes Weapon values.
+*
+* parameter: 
+*			
+* return:  none
+*/
 Weapon::Weapon(string name, int weight, int aB)
 	: Item(name, weight), attackBonus(aB)
 {
@@ -13,6 +21,14 @@ Weapon::Weapon()
 	attackBonus = 0;
 }
 
+/*
+* Weapon::displayInfo()
+* Computes in the output window Weapon, Weight and Attack Bonus.
+*
+* parameter: none 
+*
+* return:  none
+*/
 void Weapon::displayInfo() {
 	cout << "Weapon: " << name << ", Weight: " << weight << ", Attack Bonus: " << attackBonus << "\n";
 }
