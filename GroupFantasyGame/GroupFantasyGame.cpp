c19dc38ddba0083553798ae6dde349e8d74657fb
Ryan Kelly <23331682@studentmail.ul.ie>
2023-12-04 14:29:17 +0000

    Resolving conflicts

--- GroupFantasyGame/GroupFantasyGame.cpp
+++ GroupFantasyGame/GroupFantasyGame.cpp
@@ -8,43 +8,11 @@
 #include <iostream>
 #include "Game.h"
 using namespace std;
-
-	Character("Orc", 25, 10, 50, 130, 0.25, 0.25),
-};
+int main()
+{
 	Game game = Game();
 	game.run();
-
-	GameOptions(game_board, player);
-
-		case 'P':
-			// Code for command 'q'
-			std::cout << "Exiting the program." << std::endl;
-			break;
-
-		case 'D':
-			// Code for command 'q'
-			std::cout << "Exiting the program." << std::endl;
-			break;
-
-		case 'L':
-			// Code for command 'q'
-			SquareInformation(game_board);
-			break;
-
-		case 'I':
-			// Code for command 'q'
-			std::cout << "Exiting the program." << std::endl;
-			break;
-
-		case 'X':
-			exit(0);
-
-		default:
-			// Code for handling invalid input
-			std::cout << "Invalid command. Please enter a, b, c, or q." << std::endl;
-			break;
-		}
-	} while (userInput);
+}
 
 
 
